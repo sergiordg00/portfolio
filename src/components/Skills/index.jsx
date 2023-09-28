2502ce7ff226af5cb34aa5d9321e14ad05c18db2
Sergio Rodriguez <sergiordg00@gmail.com>
2023-09-28 11:24:24 +0200

    feat: add new technologies and updated cv

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,14 +1,17 @@
 import { MdWork } from "react-icons/md";
 
 import { ReactComponent as CSS } from "@/assets/icons/css.svg";
+import { ReactComponent as Cypress } from "@/assets/icons/Cypress.svg";
 import { ReactComponent as Figma } from "@/assets/icons/figma.svg";
 import { ReactComponent as Git } from "@/assets/icons/git.svg";
 import { ReactComponent as Github } from "@/assets/icons/github.svg";
 import { ReactComponent as HTML } from "@/assets/icons/html.svg";
 import { ReactComponent as JavaScript } from "@/assets/icons/javascript.svg";
+import { ReactComponent as Jest } from "@/assets/icons/jest.svg";
 import { ReactComponent as Nextjs } from "@/assets/icons/nextjs.svg";
 import { ReactComponent as React } from "@/assets/icons/react.svg";
 import { ReactComponent as Tailwind } from "@/assets/icons/tailwind.svg";
+import { ReactComponent as TypeScript } from "@/assets/icons/typescript.svg";
 import Section from "@/components/common/Section";
 
 import TechnologyCard from "./components/TechnologyCard";
@@ -88,6 +91,24 @@ export default function Skills() {
           icon={<React className="h-auto w-8 shrink-0" />}
           name="React Native"
           link="https://reactnative.dev/"
+        />
+
+        <TechnologyCard
+          icon={<TypeScript className="h-auto w-8 shrink-0" />}
+          name="TypeScript"
+          link="https://www.typescriptlang.org/"
+        />
+
+        <TechnologyCard
+          icon={<Jest className="h-auto w-8 shrink-0" />}
+          name="Jest"
+          link="https://jestjs.io/"
+        />
+
+        <TechnologyCard
+          icon={<Cypress className="h-auto w-8 shrink-0" />}
+          name="Cypress"
+          link="https://www.cypress.io/"
         />
       </div>
     </Section>
